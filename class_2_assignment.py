43b218c330912fa6c64bbd53734cdbfda49d22c8
Nahiduzzaman8 <nahiduzzamantalukder@gmail.com>
2026-01-05 00:21:07 +0600

    first assignmet completed

---
+++ class_2_assignment.py
@@ -0,0 +1,37 @@
+#1 
+print("Nahiduzzaman Talukder")
+
+#2
+batch_number = input("Enter your batch number: ")
+print("Your batch number is : ", batch_number)
+
+#3
+age = 25
+cgpa = 3.89
+course = "Django Course"
+online = True
+
+print(age)
+print(cgpa)
+print(course)
+print(online)
+
+#4
+CourseName = "Django and BackendApi Development"
+
+#5
+name = username = fullname = "Nahiduzzaman Talukder"
+print(name)
+print(username)
+print(fullname)
+
+#6
+cgpa = float(input("Enter your CGPA: "))
+
+#7
+a = "Welcome to Django for web and Ai"
+print(a.upper())
+
+#8
+b = "       Python"
+print(b.strip())
